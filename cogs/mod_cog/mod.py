4abdd4ae7cb43bcf4fb54c155ef42a047f6f6472
Dennis-Shestak <dennis.shestak.06@gmail.com>
2023-08-13 14:51:40 -0400

    added duration feature for timeout

--- cogs/mod_cog/mod.py
+++ cogs/mod_cog/mod.py
@@ -23,9 +23,45 @@ class ModCommands(commands.Cog):
             await ctx.send('You do not have permission to use this command.')
 
     @commands.command()
-    async def mute(self, ctx, member: discord.Member):
+    async def mute(self, ctx, member: discord.Member, duration="1d", *, reason="No reason lol"):
+        # time = duration.split(" ")[0] # get the FIRST time duration
+        time = duration
+        timedeltas = {
+            "weeks": 0,
+            "days": 0,
+            "hours": 0,
+            "minutes": 0
+        }
+
+        try: # check if duration format is proper
+            magnitude = int(time[:-1])
+        except ValueError:
+            await ctx.respond("Not valid duration format")
+            return
+        
+        match time[-1]:
+            case "w":
+                timedeltas["weeks"] = magnitude
+            case "d":
+                timedeltas["months"] = magnitude
+            case "h":
+                timedeltas["hours"] = magnitude
+            case "m":
+                timedeltas["minutes"] = magnitude
+            case _:
+                await ctx.respond("Not valid duration unit")
+                return
+
         if ctx.author.guild_permissions.administrator:
-            await member.timeout_for(datetime.timedelta(days=1))
+            await member.timeout_for(
+                duration=datetime.timedelta(
+                    weeks=timedeltas["weeks"],
+                    days=timedeltas["days"],
+                    hours=timedeltas["hours"],
+                    minutes=timedeltas["minutes"],
+                    ),
+                reason=reason
+                )
 
     @commands.command()
     async def unmute(self, ctx, member: discord.Member):
