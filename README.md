17d6d3a7bfa9834511caa206a5bc34228cb86436
Dennis-Shestak <dennis.shestak.06@gmail.com>
2023-08-18 17:04:35 -0400

    upgraded music commands, fixed readme

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Discord bot for Deezee Discord server.
 Includes games, moderation commands, more coming soon.
 
+## some features
+- uses cogs that are organized as packages i love organization
+- prefix commands for now
+- music commands, game commands, moderation commands
+
 ___
 ### DEPENDENCIES (if you want to run this yourself)
 
@@ -9,4 +14,15 @@ ___
 - py-cord
 - ffmpeg (music commands use the ffmpeg executable to run)
     - set the executable to be in Path on windows (user), you linux fellas can figure it out
+
+___
+### how to run the bot
+- install/download all dependencies
+- set up a `token.json` file in the main directory, containing a string containing bot token
+- run the main.py file
+
+___
+
+for any questions or you want to contribute, dm @superleaf8 on discord
+
     
--- cogs/music_cog/music.py
+++ cogs/music_cog/music.py
@@ -1,25 +1,12 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-from traceback import print_exc
-from asyncio import sleep
-import youtube_dl
-import requests
-import shutil
-import os
-from traceback import print_exc
+import pytube
+###
 import json
+import requests, os, asyncio
+from traceback import print_exc
 
-
-"""
-IF YOURE GONNA MAKE THIS BOT PUBLIC YOU NEED TO MAKE SONG FILES FOR EACH SERVER
-
-and that might suck
-
-so lay off the music commands for now
-
-SCREW YOU PAST DENNIS I MADE IT WORK
-"""
 
 # ydl_opts = {
 # 	'format': 'bestaudio/best',
@@ -108,57 +95,34 @@ class MusicCommands(commands.Cog):
 			print_exc()
 
 	@commands.command() # for fun
-	async def yt_play(self,ctx,*,song):
+	async def play(self,ctx,*,song):
 		music = get(self.bot.voice_clients,guild=ctx.guild)
 		channel = ctx.author.voice.channel
-		filename = f'{ctx.guild.id}_music.mp3'
-		ydl_opts = {
-			'format': 'bestaudio/best',
-			# 'quiet': True,
-			'outtmpl': '{0}.%(ext)s'.format(f"{ctx.guild.id}_music"),
-			'noplaylist':True,
-			'ignoreerrors':True,
-			'postprocessors': [{
-				'key': 'FFmpegExtractAudio',
-				'preferredcodec': 'mp3',
-				'preferredquality': '192',
-				}],
-		}
 
-		def search(arg):
-			with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-				try:
-					requests.get(arg)
-					video = ydl.extract_info(arg, download=False) 
-				except:
-					video = ydl.extract_info(f"ytsearch: {[arg]}", download=False)['entries'][0]
-			return video['webpage_url'], video['title']
-
-		link, title = search(song)
+		try:
+			requests.get(song)
+			yt = pytube.YouTube(song)
+		except:
+			yt = pytube.Search(song).results[0]
 		
-		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-			ydl.download([link])
-		
-		destiny = f"{os.getcwd()}/music_files/{filename}"
-		source = f"{os.getcwd()}/{filename}"
-		
-
-		shutil.move(source,destiny)
-
-		print("\n\ndownloaded",title)
+		stream = yt.streams.filter(only_audio=True).first()
+		destiny = stream.download(filename=f"./cogs/music_cog/music_cache/{str(ctx.guild.id)}")
 		
 		audio = discord.FFmpegPCMAudio(source=destiny)
+
 		if not channel:
 			await ctx.send("youre not in a channel")
 			return
 		if not music:
 			await ctx.send("am not in channel")
 			return
+
 		def replay():
-			source = discord.FFmpegPCMAudio(source=f"{os.getcwd()}/music_files/{filename}")
+			source = discord.FFmpegPCMAudio(source=destiny)
 			if str(ctx.guild.id) in self.loops:		
 				music.play(source,after=lambda bruh: replay()) # THIS IS FUCKING CRASHING
 				music.source = discord.PCMVolumeTransformer(music.source,volume=self.volumes.get(ctx.guild.id,1.0))
+		
 		if music.is_playing():
 			music.stop()
 		music.play(audio,after=lambda check: replay())
@@ -191,58 +155,7 @@ class MusicCommands(commands.Cog):
 		channel = ctx.author.voice.channel
 		music.play(source = discord.FFmpegPCMAudio(source=f"music_files/{ctx.guild.id}_music.mp3"))
 		music.source = discord.PCMVolumeTransformer(music.source,volume=1.0)
-	
-	# @commands.command()
-	# async def play(self, ctx,*,name):
-	# 	music = get(self.bot.voice_clients,guild=ctx.guild)
-	# 	channel = ctx.author.voice.channel
-	# 	if str(ctx.guild.id) in self.loops:
-	# 		self.loops.remove(str(ctx.guild.id))
-	# 	with open("inventory.json","r") as f:
-	# 		data = json.load(f)
-	# 		uitems = [] # the items the user actually has
-	# 		for di in data[str(ctx.author.id)]:
-	# 			uitem = item.Item(di["name"],di["type"],di["price"])
-	# 			uitems.append(uitem)
-			
-	# 		exists = False
-	# 		is_music = False
-	# 		for i in item.all_items:
-	# 			if i.name == name:
-	# 				exists = True
-	# 				if i.itemtype == "music":
-	# 					is_music = True
-	# 		if not exists:
-	# 			await ctx.send("this item doesnt even exist")
-	# 			return
-	# 		elif not is_music:
-	# 			await ctx.send("this isnt a music item")
-	# 			return
-
-	# 		if not name in [n.name for n in uitems]:
-	# 			await ctx.send("you dont have this song")
-	# 			return
-
-	# 	file = f"{os.getcwd()}\\kk_music\\{name}.mp3"
 		
-	# 	audio = discord.FFmpegPCMAudio(source=file)
-	# 	if not channel:
-	# 		await ctx.send("youre not in a channel")
-	# 		return
-	# 	if not music:
-	# 		await ctx.send("am not in channel")
-	# 		return
-	# 	def replay():
-	# 		source = discord.FFmpegPCMAudio(source=file)
-	# 		if str(ctx.guild.id) in self.loops:		
-	# 			music.play(source,after=lambda bruh: replay()) # THIS IS FUCKING CRASHING
-	# 			music.source = discord.PCMVolumeTransformer(music.source,volume=self.volumes.get(ctx.guild.id,1.0))
-	# 	if music.is_playing():
-	# 		music.stop()
-	# 	music.play(audio,after=lambda check: replay())
-	# 	music.source = discord.PCMVolumeTransformer(music.source,volume=self.volumes.get(ctx.guild.id,1.0))
-	# 	await ctx.send(f"playing {name}")
-	
 	@commands.command()
 	async def loop(self, ctx): # can only loop when music play
 		music = get(self.bot.voice_clients,guild=ctx.guild)
